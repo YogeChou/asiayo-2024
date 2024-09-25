dac23b8a48f598a54cf1c1bc56816ecca0b7d97f
Yoge <jimbensonz@owlsome.tech>
2024-09-26 01:26:31 +0800

    modify success http code

--- app/routes/order_routes.py
+++ app/routes/order_routes.py
@@ -6,7 +6,7 @@ from app.service import CurrencyService
 order_router = APIRouter(tags=['Order'])
 
 
-@order_router.post("/", status_code=201)
+@order_router.post("/", status_code=200)
 async def validate_order(order: OrderSchema, currency_service: CurrencyService = Depends(CurrencyService)):
     order.price *= currency_service.get_rate(order.currency)
     order.currency = "TWD"
--- tests/test_order_api.py
+++ tests/test_order_api.py
@@ -1,7 +1,7 @@
 
 def test_post_orders_ok(test_client, order):
     response = test_client.post("/api/orders", json=order)
-    assert response.status_code == 201
+    assert response.status_code == 200
 
 
 def test_post_orders_invalid_request(test_client, order):
